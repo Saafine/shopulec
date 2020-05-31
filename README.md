588b03e009efce11853dd0224dcb865d222622d6
Labus, Pawel <pawel.labus@santander.pl>
2020-05-31 17:19:16 +0200

    X

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-deploy
 better populate store on init
+
 switching tabs by swiping left and right
+
 drag and drop
 
--- src/core/PredefinedShopItems.ts
+++ src/core/PredefinedShopItems.ts
@@ -1,5 +1,59 @@
 import { ShopItem } from '../components/ShopItems'
 
-export const PREDEFINED_SHOP_ITEMS: ShopItem[] = [
+const values = [
+  'Chleb / Bułki',
+  'Jogurty Skyrr',
+  'Śmietana',
+  'Płatki orkiszowe',
+  'Indyk',
+  'Jajka',
+  'Ser',
+  'Dżem',
+  'Szynka',
+  'Kiełbasa',
+  'Orzechy',
+  'Twaróg',
+  'Suszone pomidory',
+
+  // Owoce i warzywa
+  'Banany',
+  'Marchewki',
+  'Cebula',
+  'Papryka',
+  'Rzodkiewka',
+  'Ziemniaki',
+  'Pomidory',
+  'Pomidory w puszce',
+  'Ogórek',
+  'Miks sałat',
   'Mrożone owoce',
-].map((label) => ({ label, value: false }))
+  'Szpinak',
+  'Imbir',
+  'Buraczki',
+  'Sałata',
+
+  // Napoje
+  'Mleko migdałowe',
+  'Mleko czekoladowe',
+  'Woda',
+  'Mleko',
+  'Coca Cola',
+  'Herbata',
+
+  // Chemia i czystość
+  'Ręczniki papierowe',
+  'Mokre ściereczki',
+  'Płyn do naczyń',
+  'Płyn do prania',
+  'Worki na śmieci',
+  'Papier do wypieków',
+
+  // Przyprawy
+  'Sól',
+  'Pieprz',
+]
+
+export const PREDEFINED_SHOP_ITEMS: ShopItem[] = values.map((label) => ({
+  label,
+  value: false,
+}))
