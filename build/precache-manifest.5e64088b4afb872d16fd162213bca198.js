a3268d5e1f18f6f696aa60cba89e7f666cabc04b
Labus, Pawel <pawel.labus@santander.pl>
2020-05-31 16:54:02 +0200

    X

---
+++ build/precache-manifest.5e64088b4afb872d16fd162213bca198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17860adbb0da0dbe568e6755fee2544c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6b95fbee087918a89be",
+    "url": "/static/css/main.630e10ea.chunk.css"
+  },
+  {
+    "revision": "5fbf3f13aed77f35f9d4",
+    "url": "/static/js/2.a286d9c2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a286d9c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b95fbee087918a89be",
+    "url": "/static/js/main.7ecd9088.chunk.js"
+  },
+  {
+    "revision": "31367fdf1a1479e63162",
+    "url": "/static/js/runtime-main.87a06284.js"
+  }
+]);
